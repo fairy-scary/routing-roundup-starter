7ee4ecfe40c73a4113634b7827959846a7a1282e
fairy-scary <ecoautumn@gmail.com>
2020-10-20 15:16:08 -0500

    finished phase 3

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require ('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
+app.get('/about', (req, res) => {
+    res.render('layout', {method: req.method, path: req.path, randNum: 42});
+ 
+   
+})
+
+app.get('/*xyz', (req, res) => {
+    res.render('layout', {method: req.method, path: req.path, randNum: 42});
+    res.send("That's all I wrote!")
+})
+
 app.get('/', (req, res) => {
     res.send("Hello from Express!");
+
 })
 
 const port = 8081
